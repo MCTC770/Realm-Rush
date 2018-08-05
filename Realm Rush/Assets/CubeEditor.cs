7d84a48744aa7b85dad6fe91e30e20c372dc1770
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-08-05 18:11:22 +0200

    Show coordinates on cube

--- Realm Rush/Assets/CubeEditor.cs
+++ Realm Rush/Assets/CubeEditor.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
 	[Tooltip ("The size of a tile in the grid")][Range(1f, 20f)][SerializeField] float gridSize = 10.0f;
 
+	TextMesh textMesh;
+
 	void Update()
 	{
-		gridSize = Mathf.RoundToInt(gridSize);
 		Vector3 snapPosition;
+
+		gridSize = Mathf.RoundToInt(gridSize);
 		snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 		transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = "X: " + snapPosition.x / gridSize + "\nZ: " + snapPosition.z / gridSize;
 	}
 }
