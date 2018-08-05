cf633d87f0e98c4b20f2c80698da5f15c9bee94b
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-08-05 17:38:31 +0200

    Integrated grid system

---
+++ Realm Rush/Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+	[Tooltip ("The size of a tile in the grid")][Range(1f, 20f)][SerializeField] float gridSize = 10.0f;
+
+	void Update()
+	{
+		gridSize = Mathf.RoundToInt(gridSize);
+		Vector3 snapPosition;
+		snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+		snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+		transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+	}
+}
