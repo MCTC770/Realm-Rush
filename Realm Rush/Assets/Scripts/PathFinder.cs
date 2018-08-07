c1c38fceb6ae7b7cf3b5c033a07e54b66ac9ddf0
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-08-07 23:37:24 +0200

    added queue to pathfinding script

--- Realm Rush/Assets/Scripts/PathFinder.cs
+++ Realm Rush/Assets/Scripts/PathFinder.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour {
 
 	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+	Queue<Waypoint> queue = new Queue<Waypoint>();
+	bool isRunning = true;
+
 	Vector2Int[] directions =
 	{
 		Vector2Int.up,
@@ -27,7 +30,8 @@ public class PathFinder : MonoBehaviour {
 	void Start () {
 		LoadBlocks();
 		ColorStartAndEnd();
-		ExploreNeighbors();
+		PathFind();
+		//ExploreNeighbors();
 	}
 
 	private void LoadBlocks()
@@ -52,6 +56,28 @@ public class PathFinder : MonoBehaviour {
 	{
 		startWaypoint.SetTopColor(startColor);
 		endWaypoint.SetTopColor(endColor);
+	}
+
+	void PathFind()
+	{
+		queue.Enqueue(startWaypoint);
+
+		while (queue.Count > 0)
+		{
+			var searchCenter = queue.Dequeue();
+			print("Searching from: " + searchCenter);
+			HaltIfEndFound(searchCenter);
+		}
+		print("Finished pathfinding?");
+	}
+
+	private void HaltIfEndFound(Waypoint searchCenter)
+	{
+		if (searchCenter == endWaypoint)
+		{
+			print("Searching from end node, therefore stopping.");
+			isRunning = false;
+		}
 	}
 
 	void ExploreNeighbors()
