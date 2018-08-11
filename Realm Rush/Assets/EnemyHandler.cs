28dc5b704d707e547b5b9c9ea6b37355d9ead1ff
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-08-11 23:31:07 +0200

    Gun tower shoots lasers; lasers damage enemies; enemies can die

---
+++ Realm Rush/Assets/EnemyHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour {
+
+	[SerializeField] int enemyHP = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnParticleCollision(GameObject other)
+	{
+		enemyHP -= 1;
+		EnemyHit();
+	}
+
+	private void EnemyHit()
+	{
+		if (enemyHP <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
