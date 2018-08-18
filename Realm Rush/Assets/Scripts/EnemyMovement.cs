e1cf2a65a18a8132fc7e5089bec48bed2a0bac81
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-08-18 19:30:30 +0200

    make movement smooth finished

--- Realm Rush/Assets/Scripts/EnemyMovement.cs
+++ Realm Rush/Assets/Scripts/EnemyMovement.cs
@@ -10,6 +10,8 @@ public class EnemyMovement: MonoBehaviour {
 	float moveStep = 0f;
 	Vector3 startPosition;
 	Vector3 targetPosition;
+	Quaternion startRotation;
+	Quaternion targetRotation;
 
 	// Use this for initialization
 	void Start () {
@@ -24,12 +26,18 @@ public class EnemyMovement: MonoBehaviour {
 		{
 			startPosition = new Vector3(0, -10, 10);
 		}
+
 		print(startPosition + " and " + targetPosition);
 		transform.position = Vector3.Lerp(startPosition, targetPosition, moveStep);
+		transform.rotation = Quaternion.Slerp(startRotation, targetRotation, moveStep * 2);
 		moveStep += Time.deltaTime;
 		if (moveStep >= timeTicks)
 		{
-			startPosition = transform.position;
+			float x = Mathf.Round(transform.position.x);
+			float y = Mathf.Round(transform.position.y);
+			float z = Mathf.Round(transform.position.z);
+			startPosition = new Vector3(x,y,z);
+			startRotation = transform.rotation;
 			moveStep = 0f;
 		}
 	}
@@ -39,10 +47,8 @@ public class EnemyMovement: MonoBehaviour {
 		print("Starting patrol...");
 		foreach (Waypoint waypoint in path)
 		{
-			startPosition = transform.position;
 			targetPosition = waypoint.transform.position + new Vector3(0, offset, 0);
-			//transform.position = waypoint.transform.position;
-			//transform.position += new Vector3 (0, offset, 0);
+			targetRotation = Quaternion.LookRotation((targetPosition - transform.position), Vector3.up);
 			yield return new WaitForSeconds(timeTicks);
 		}
 		print("Ending patrol");
